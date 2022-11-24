d820e41028cbc8463547445545558f8780f1cf54
Ashraful Islam <ashrafulcse.ru@gmail.com>
2022-11-24 19:20:06 +0600

    comment deleted

--- StudentList.java
+++ StudentList.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class StudentList {
 	public static void main(String[] args) {
 
-//		Check arguments
 		if(args[0].equals("a")) {
 			System.out.println("Loading data ...");			
 			try {
