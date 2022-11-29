0267a875e34ebd238f42199f8e893c5eb1a4bee1
Zannatul Naim <zannatulnaim09@gmail.com>
2022-11-30 00:10:15 +0600

    Update StudentList.java

--- StudentList.java
+++ StudentList.java
@@ -1,84 +1,101 @@
 import java.io.*;
+import java.lang.constant.Constable;
 import java.text.*;
 import java.util.*;
 
 public class StudentList {
 	public static void main(String[] args) {
-
-		// Check arguments
-		if (args.length != 1) { //Too many arguments
-			System.out.println(Constants.USAGE_MESSAGE); //Usage message
+		if (args.length != 1) { // Check number of arguments
+			System.out.println(Constants.warningMessage);
 		} else {
-			if (args[0].equals(Constants.SHOW_ALL)) { //Show all students of the list
-				System.out.println(Constants.LOADING_DATA);
-				String names[] = readLineFromFile(Constants.FILE_NAME).split(Constants.COMMA);
+			if (args[0].equals(Constants.showAll)) { // Show all students name in student.txt file
+				System.out.println(Constants.loadData);
+				String names[] = readFromFile(Constants.studentList);
 				for (String name : names) {
 					System.out.println(name.trim());
 				}
-				System.out.println(Constants.LOADING_DATA);
-			} else if (args[0].equals(Constants.RANDOM_NUMBER)) { //Show a random student
-				System.out.println(Constants.DATA_LOADED);
-				String names[] = readLineFromFile(Constants.FILE_NAME).split(Constants.COMMA);
+				System.out.println(Constants.dataLoaded);
+
+			} else if (args[0].equals(Constants.randomName)) { // Show a random student name
+				System.out.println(Constants.loadData);
+				String names[] = readFromFile(Constants.studentList);
 				Random random = new Random();
 				System.out.println(names[random.nextInt(4)].trim());
-				System.out.println(Constants.LOADING_DATA);
-			} else if (args[0].contains(Constants.ADD_STUDENT)) { //Add a new student
-				System.out.println(Constants.LOADING_DATA);
-				try {
-					BufferedWriter bufferedWriter = writeFile(Constants.FILE_NAME);
-					String newStudent = args[0].substring(1);
-					DateFormat dateFormat = new SimpleDateFormat(Constants.DATE_TIME_FORMAT);
-					String dateToday = dateFormat.format(new Date());
-					bufferedWriter.write(Constants.COMMA + newStudent + Constants.LIST_UPDATE_MESSAGE + dateToday);
-					System.out.println(newStudent + Constants.ADDED_MESSAGE);
-					bufferedWriter.close();
-				} catch (Exception e) {
-				}
-				System.out.println(Constants.LOADING_DATA);
-			} else if (args[0].contains(Constants.FIND_STUDENT)) { //Find a student
-				System.out.println(Constants.LOADING_DATA);
-				String names[] = readLineFromFile(Constants.FILE_NAME).split(Constants.COMMA);
+				System.out.println(Constants.dataLoaded);
+
+			} else if (args[0].contains(Constants.addName)) { // Add a new student
+				System.out.println(Constants.loadData);
+				String newStudentName = args[0].substring(1);
+				DateFormat dateFormat = new SimpleDateFormat(Constants.dateFormat);
+				String formatedDate = dateFormat.format(new Date());
+				deleteUpdateMessage(Constants.studentList);
+				writeFile(Constants.studentList,
+						Constants.comma + newStudentName + Constants.listUpdateMessage + formatedDate);
+				System.out.println(newStudentName + Constants.addedMessage);
+				System.out.println(Constants.dataLoaded);
+
+			} else if (args[0].contains(Constants.findName)) { // find a name along with its occurrances
+				System.out.println(Constants.loadData);
+				String[] names = readFromFile(Constants.studentList);
 				int count = 0;
-				String student = args[0].substring(1);
-				for (int index = 0; index < names.length; index++) {
-					if (names[index].trim().equals(student)) {
+				for (int i = 0; i < names.length; i++) {
+					if (names[i].trim().equals(args[0].substring(1))) {
 						count++;
 					}
 				}
 				if (count == 0) {
-					System.out.println(Constants.NOT_FOUND_MESSAGE);
+					System.out.println(args[0].substring(1) + Constants.notFoundMessage);
+				} else {
+					System.out.println(args[0].substring(1) + Constants.foundMessage + " " + count + " time(s)");
 				}
-				else {
-					System.out.println(Constants.FOUND_MESSAGE + Constants.SPACE + count + Constants.TIMES);
-				}
-				System.out.println(Constants.LOADING_DATA);
-			} else if (args[0].contains(Constants.COUNT_STUDENT)) { //Count number of students
-				System.out.println(Constants.LOADING_DATA);
-				String names[] = readLineFromFile(Constants.FILE_NAME).split(Constants.COMMA);
-				System.out.println(names.length + Constants.WORD_COUNT_MESSAGE);
-				System.out.println(Constants.LOADING_DATA);
+				System.out.println(Constants.dataLoaded);
+
+			} else if (args[0].equals(Constants.countStudents)) { // count total number of students
+				System.out.println(Constants.loadData);
+				String[] names = readFromFile(Constants.studentList);
+				System.out.println(names.length + Constants.wordFoundMessage);
+				System.out.println(Constants.dataLoaded);
 			} else {
-				System.out.println(Constants.USAGE_MESSAGE); //Usage message incase of invalid arguments
+				System.out.println(Constants.warningMessage); // Warning: when argument is invalid
 			}
 		}
-
 	}
 
-	static String readLineFromFile(String fileName) { //File reader
+	static String[] readFromFile(String fileName) {
 		try {
 			BufferedReader bufferedReader = new BufferedReader(
-					new InputStreamReader(new FileInputStream(fileName)));
-			return bufferedReader.readLine();
+					new InputStreamReader(
+							new FileInputStream(fileName)));
+			String students[] = bufferedReader.readLine().split(Constants.comma);
+			bufferedReader.close();
+			return students;
+
 		} catch (Exception e) {
 			return null;
 		}
 	}
 
-	static BufferedWriter writeFile(String fileName) { //File writer
+	public static void writeFile(String fileName, String studentName) {
 		try {
-			return new BufferedWriter(new FileWriter(fileName, true));
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true));
+			bufferedWriter.write(studentName);
+			bufferedWriter.close();
 		} catch (Exception e) {
-			return null;
+		}
+	}
+
+	public static void deleteUpdateMessage(String fileName) {
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+			String studentList = bufferedReader.readLine();
+			bufferedReader.close();
+			File newFile = new File("temp.txt");
+			File oldFile = new File("students.txt");
+			newFile.createNewFile();
+			writeFile("temp.txt", studentList);
+			oldFile.delete();
+			newFile.renameTo(oldFile);
+		} catch (Exception e) {
 		}
 	}
 }
