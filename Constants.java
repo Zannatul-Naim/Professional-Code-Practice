2cfac855d94821f194371416ddea5ccf43870cf7
Zannatul Naim <zannatulnaim09@gmail.com>
2022-11-30 00:10:49 +0600

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,20 +1,18 @@
 public abstract class Constants {
-    public static String FILE_NAME = "students.txt";
-    public static String SHOW_ALL = "a";
-    public static String RANDOM_NUMBER = "r";
-    public static String ADD_STUDENT = "+";
-    public static String FIND_STUDENT = "?";
-    public static String COUNT_STUDENT = "c";
-    public static String COMMA = ", ";
-    public static String FOUND_MESSAGE = "We found it";
-    public static String NOT_FOUND_MESSAGE = "We didn't fiund it!";
-    public static String WORD_COUNT_MESSAGE = " word(s) found";
-    public static String DATE_TIME_FORMAT = "dd/MM/yyyy-hh:mm:ss a";
-    public static String LIST_UPDATE_MESSAGE = "\nList last updated on ";
-    public static String USAGE_MESSAGE = "\nUsage: a | r | c | +Student | ?Student\n";
-    public static String LOADING_DATA = "Loading data ...";
-    public static String DATA_LOADED = "Data Loaded.";
-    public static String ADDED_MESSAGE = " is added to students.txt";
-    public static String SPACE = " ";
-    public static String TIMES = " time(s)";
+    public static String studentList = "students.txt";
+    public static String showAll = "a";
+    public static String randomName = "r";
+    public static String addName = "+";
+    public static String findName = "?";
+    public static String countStudents = "c";
+    public static String comma = ", ";
+    public static String foundMessage = " found";
+    public static String notFoundMessage = " not found!";
+    public static String wordFoundMessage = " word(s) found";
+    public static String dateFormat = "dd/MM/yyyy-hh:mm:ss a";
+    public static String listUpdateMessage = "\nList last updated on ";
+    public static String warningMessage = "\nUsage: a | r | c | +StudentName | ?StudentName\n";
+    public static String loadData = "Loading data ...";
+    public static String dataLoaded = "Data Loaded.";
+    public static String addedMessage = " is added to students.txt";
 }
